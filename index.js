9f0a6f3c7e83f5b0312c7f9e41bc3482794f2fd6
Mohit <mohitshah7777@gmail.com>
2021-11-09 14:07:39 +0530

    uploaded to staging

--- index.js
+++ index.js
@@ -82,5 +82,5 @@ let server = app.listen(3000, function () {
     let host = server.address().address;
     let port = server.address().port;
 
-    console.log("App listening at http://%s:%s", host, port);
+    console.log("App listening at ", host, port);
 })
